102f45f2e6215e33cb6204e8c9039780a908658d
EmsCode82 <emsgithub@gmail.com>
2021-11-01 19:36:53 -0500

    .

---
+++ first_program.py
@@ -0,0 +1,15 @@
+#Python Course
+#Eric Shepard
+#LearnToProgram.tv
+
+        #Set Variables
+        name = "Python for Beginners 2017"
+        age = 39
+        student = "Eric Shepard"
+
+        #Output
+        print (name)
+        print (age)
+        print (student)
+
+
